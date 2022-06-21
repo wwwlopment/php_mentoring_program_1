0952a33080cc986d1a6a3d599b9d9b8ff8dcfe9f
Stochanskiy Bogdan <36885425+wwwlopment@users.noreply.github.com>
2022-06-21 15:33:23 +0300

    Create reverse_array.php

---
+++ reverse_array.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ * Complete the 'reverseArray' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER_ARRAY a as parameter.
+ */
+
+function reverseArray($a) {
+    // Write your code here
+
+}
+
+$fptr = fopen(getenv("OUTPUT_PATH"), "w");
+
+$arr_count = intval(trim(fgets(STDIN)));
+
+$arr_temp = rtrim(fgets(STDIN));
+
+$arr = array_map('intval', preg_split('/ /', $arr_temp, -1, PREG_SPLIT_NO_EMPTY));
+
+$res = reverseArray($arr);
+
+fwrite($fptr, implode(" ", $res) . "\n");
+
+fclose($fptr);
