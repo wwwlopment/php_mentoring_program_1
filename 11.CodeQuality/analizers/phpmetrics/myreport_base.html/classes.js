e93be163ce3f0dabfea5d2a21c34d861498d3122
Bogdan <wwwlopment@gmail.com>
2022-09-05 22:35:50 +0300

    analyse

---
+++ 11.CodeQuality/analizers/phpmetrics/myreport_base.html/classes.js
@@ -0,0 +1,372 @@
+var classes = [
+    {
+        "name": "Text",
+        "interface": false,
+        "abstract": false,
+        "final": false,
+        "methods": [
+            {
+                "name": "__construct",
+                "role": "setter",
+                "_type": "Hal\\Metric\\FunctionMetric"
+            },
+            {
+                "name": "getCharactersCount",
+                "role": null,
+                "public": true,
+                "private": false,
+                "_type": "Hal\\Metric\\FunctionMetric"
+            },
+            {
+                "name": "getWordsCount",
+                "role": null,
+                "public": true,
+                "private": false,
+                "_type": "Hal\\Metric\\FunctionMetric"
+            },
+            {
+                "name": "getSentencesCount",
+                "role": null,
+                "public": true,
+                "private": false,
+                "_type": "Hal\\Metric\\FunctionMetric"
+            },
+            {
+                "name": "getCharactersFrequency",
+                "role": null,
+                "public": true,
+                "private": false,
+                "_type": "Hal\\Metric\\FunctionMetric"
+            },
+            {
+                "name": "getCharactersPercentage",
+                "role": null,
+                "public": true,
+                "private": false,
+                "_type": "Hal\\Metric\\FunctionMetric"
+            },
+            {
+                "name": "getWordLengthAverage",
+                "role": null,
+                "public": true,
+                "private": false,
+                "_type": "Hal\\Metric\\FunctionMetric"
+            },
+            {
+                "name": "getWordsInSentenceAverage",
+                "role": null,
+                "public": true,
+                "private": false,
+                "_type": "Hal\\Metric\\FunctionMetric"
+            },
+            {
+                "name": "getReverseText",
+                "role": null,
+                "public": true,
+                "private": false,
+                "_type": "Hal\\Metric\\FunctionMetric"
+            },
+            {
+                "name": "getReverseWords",
+                "role": null,
+                "public": true,
+                "private": false,
+                "_type": "Hal\\Metric\\FunctionMetric"
+            },
+            {
+                "name": "isPalindromeString",
+                "role": null,
+                "public": true,
+                "private": false,
+                "_type": "Hal\\Metric\\FunctionMetric"
+            },
+            {
+                "name": "getPalindromeWordsCount",
+                "role": null,
+                "public": true,
+                "private": false,
+                "_type": "Hal\\Metric\\FunctionMetric"
+            },
+            {
+                "name": "isWholeTextPalindrome",
+                "role": null,
+                "public": true,
+                "private": false,
+                "_type": "Hal\\Metric\\FunctionMetric"
+            },
+            {
+                "name": "getSentencesArray",
+                "role": null,
+                "public": true,
+                "private": false,
+                "_type": "Hal\\Metric\\FunctionMetric"
+            },
+            {
+                "name": "getWordsArray",
+                "role": null,
+                "public": true,
+                "private": false,
+                "_type": "Hal\\Metric\\FunctionMetric"
+            },
+            {
+                "name": "getMostUsedWords",
+                "role": null,
+                "public": true,
+                "private": false,
+                "_type": "Hal\\Metric\\FunctionMetric"
+            },
+            {
+                "name": "getShortestWords",
+                "role": null,
+                "public": true,
+                "private": false,
+                "_type": "Hal\\Metric\\FunctionMetric"
+            },
+            {
+                "name": "getLongestWords",
+                "role": null,
+                "public": true,
+                "private": false,
+                "_type": "Hal\\Metric\\FunctionMetric"
+            },
+            {
+                "name": "getShortestSentences",
+                "role": null,
+                "public": true,
+                "private": false,
+                "_type": "Hal\\Metric\\FunctionMetric"
+            },
+            {
+                "name": "getLongestSentences",
+                "role": null,
+                "public": true,
+                "private": false,
+                "_type": "Hal\\Metric\\FunctionMetric"
+            },
+            {
+                "name": "getLongestPalindromeWords",
+                "role": null,
+                "public": true,
+                "private": false,
+                "_type": "Hal\\Metric\\FunctionMetric"
+            },
+            {
+                "name": "sortedByLength",
+                "role": null,
+                "public": false,
+                "private": true,
+                "_type": "Hal\\Metric\\FunctionMetric"
+            },
+            {
+                "name": "strrevArr",
+                "role": null,
+                "public": false,
+                "private": true,
+                "_type": "Hal\\Metric\\FunctionMetric"
+            }
+        ],
+        "nbMethodsIncludingGettersSetters": 23,
+        "nbMethods": 22,
+        "nbMethodsPrivate": 2,
+        "nbMethodsPublic": 20,
+        "nbMethodsGetter": 0,
+        "nbMethodsSetters": 1,
+        "wmc": 39,
+        "ccn": 18,
+        "ccnMethodMax": 4,
+        "externals": [],
+        "parents": [],
+        "implements": [],
+        "lcom": 1,
+        "length": 313,
+        "vocabulary": 58,
+        "volume": 1833.55,
+        "difficulty": 25.74,
+        "effort": 47204.11,
+        "level": 0.04,
+        "bugs": 0.61,
+        "time": 2622,
+        "intelligentContent": 71.22,
+        "number_operators": 93,
+        "number_operands": 220,
+        "number_operators_unique": 11,
+        "number_operands_unique": 47,
+        "cloc": 87,
+        "loc": 275,
+        "lloc": 188,
+        "mi": 63.38,
+        "mIwoC": 25.12,
+        "commentWeight": 38.26,
+        "kanDefect": 1.75,
+        "relativeStructuralComplexity": 64,
+        "relativeDataComplexity": 3.49,
+        "relativeSystemComplexity": 67.49,
+        "totalStructuralComplexity": 1472,
+        "totalDataComplexity": 80.33,
+        "totalSystemComplexity": 1552.33,
+        "package": "\\",
+        "pageRank": 0.21,
+        "afferentCoupling": 0,
+        "efferentCoupling": 0,
+        "instability": 0,
+        "violations": {}
+    },
+    {
+        "name": "TimeService",
+        "interface": false,
+        "abstract": false,
+        "final": false,
+        "methods": [
+            {
+                "name": "__construct",
+                "role": null,
+                "public": true,
+                "private": false,
+                "_type": "Hal\\Metric\\FunctionMetric"
+            },
+            {
+                "name": "getSpentTime",
+                "role": null,
+                "public": true,
+                "private": false,
+                "_type": "Hal\\Metric\\FunctionMetric"
+            },
+            {
+                "name": "getFormattedSpentTime",
+                "role": null,
+                "public": true,
+                "private": false,
+                "_type": "Hal\\Metric\\FunctionMetric"
+            },
+            {
+                "name": "getCurrentFormattedDataTime",
+                "role": null,
+                "public": true,
+                "private": false,
+                "_type": "Hal\\Metric\\FunctionMetric"
+            },
+            {
+                "name": "getCurrentTime",
+                "role": null,
+                "public": true,
+                "private": false,
+                "_type": "Hal\\Metric\\FunctionMetric"
+            }
+        ],
+        "nbMethodsIncludingGettersSetters": 5,
+        "nbMethods": 5,
+        "nbMethodsPrivate": 0,
+        "nbMethodsPublic": 5,
+        "nbMethodsGetter": 0,
+        "nbMethodsSetters": 0,
+        "wmc": 5,
+        "ccn": 1,
+        "ccnMethodMax": 1,
+        "externals": [],
+        "parents": [],
+        "implements": [],
+        "lcom": 2,
+        "length": 19,
+        "vocabulary": 11,
+        "volume": 65.73,
+        "difficulty": 4.58,
+        "effort": 301.26,
+        "level": 0.22,
+        "bugs": 0.02,
+        "time": 17,
+        "intelligentContent": 14.34,
+        "number_operators": 8,
+        "number_operands": 11,
+        "number_operators_unique": 5,
+        "number_operands_unique": 6,
+        "cloc": 18,
+        "loc": 44,
+        "lloc": 26,
+        "mi": 98.1,
+        "mIwoC": 56.27,
+        "commentWeight": 41.83,
+        "kanDefect": 0.15,
+        "relativeStructuralComplexity": 4,
+        "relativeDataComplexity": 1.4,
+        "relativeSystemComplexity": 5.4,
+        "totalStructuralComplexity": 20,
+        "totalDataComplexity": 7,
+        "totalSystemComplexity": 27,
+        "package": "\\",
+        "pageRank": 0.58,
+        "afferentCoupling": 1,
+        "efferentCoupling": 0,
+        "instability": 0,
+        "violations": {}
+    },
+    {
+        "name": "MainController",
+        "interface": false,
+        "abstract": false,
+        "final": false,
+        "methods": [
+            {
+                "name": "__construct",
+                "role": null,
+                "public": true,
+                "private": false,
+                "_type": "Hal\\Metric\\FunctionMetric"
+            },
+            {
+                "name": "actionIndex",
+                "role": null,
+                "public": true,
+                "private": false,
+                "_type": "Hal\\Metric\\FunctionMetric"
+            }
+        ],
+        "nbMethodsIncludingGettersSetters": 2,
+        "nbMethods": 2,
+        "nbMethodsPrivate": 0,
+        "nbMethodsPublic": 2,
+        "nbMethodsGetter": 0,
+        "nbMethodsSetters": 0,
+        "wmc": 4,
+        "ccn": 3,
+        "ccnMethodMax": 3,
+        "externals": [
+            "TimeService",
+            "App\\models\\Text"
+        ],
+        "parents": [],
+        "implements": [],
+        "lcom": 2,
+        "length": 19,
+        "vocabulary": 13,
+        "volume": 70.31,
+        "difficulty": 2.44,
+        "effort": 171.86,
+        "level": 0.41,
+        "bugs": 0.02,
+        "time": 10,
+        "intelligentContent": 28.76,
+        "number_operators": 8,
+        "number_operands": 11,
+        "number_operators_unique": 4,
+        "number_operands_unique": 9,
+        "cloc": 6,
+        "loc": 24,
+        "lloc": 18,
+        "mi": 94.25,
+        "mIwoC": 59.28,
+        "commentWeight": 34.97,
+        "kanDefect": 0.22,
+        "relativeStructuralComplexity": 0,
+        "relativeDataComplexity": 0,
+        "relativeSystemComplexity": 0,
+        "totalStructuralComplexity": 0,
+        "totalDataComplexity": 0,
+        "totalSystemComplexity": 0,
+        "package": "\\",
+        "pageRank": 0.21,
+        "afferentCoupling": 0,
+        "efferentCoupling": 2,
+        "instability": 1,
+        "violations": {}
+    }
+]
